f54802cdb6bb57527250c72e3d6cfb11642ee56c
Kevin Gallo <Gallok731@gmail.com>
2024-12-11 19:44:20 -0500

    Inital Bank Code

---
+++ contracts/bank_contract.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Bank{
+    mapping(address => uint) private account_balance;
+    mapping(address => uint[]) private blocks;
+
+    function deposit_funds() public payable {
+        account_balance[msg.sender] += msg.value;
+        blocks[msg.sender].push(block.number);
+    }
+
+    function withdraw_funds(uint _funds) public {
+        require(account_balance[msg.sender] >= _funds, "Insufficient Funds");
+        account_balance[msg.sender] -= _funds;
+
+        (bool success, ) = msg.sender.call{value: _funds}("Amount Withdrawn");
+        require(success, "Insufficient Funds");
+        blocks[msg.sender].push(block.number);
+    }
+
+    function transfer_funds(address payable receiving_address, uint _funds) public{
+        require(account_balance[msg.sender] >= _funds, "Insufficient Funds");
+
+        account_balance[msg.sender] -= _funds;
+        account_balance[receiving_address] += _funds;
+        blocks[msg.sender].push(block.number);
+        blocks[receiving_address].push(block.number);
+    }
+
+    function check_balance() public view returns (uint) {
+        return account_balance[msg.sender];
+    }
+}
